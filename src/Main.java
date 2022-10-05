c79b9e2e2fb0a807499aae52f13e747f90e3517d
Gabriel Sene <sene300@gmail.com>
2022-10-05 19:30:33 -0300

    teste

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,40 @@
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Começo");
-		metodo1();
+		try {
+			metodo1();
+		} catch (ArithmeticException ex) {
+			String msg = ex.getMessage();
+			System.out.println("ArithmeticException" + msg);
+
+		}
+		
 		System.out.println("Finish");
 		
-		
-		
+
 	}
-	
+
 	public static void metodo1() {
 		System.out.println("1");
 		metodo2();
 		metodo3();
 	}
-	
+
 	public static void metodo2() {
 		System.out.println("2");
-		
-		try {
-			
-		}catch(ArithmeticException ex){
-			String msg = ex.getMessage();
-			System.out.println("ArithmeticException"+ msg);
-			
-		}
+
 	}
-	
+
 	public static void metodo3() {
-		for(int i=3;i<=5;i++) {
+		for (int i = 3; i <= 5; i++) {
 			System.out.println(i);
-			int g=1/0;
-			
-			
+			int g = 1 / 0;
+
+
 		}
-		
-		
-		
+
 	}
 
 }
